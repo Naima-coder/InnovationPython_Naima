81a911c19da310b4b377fc41d2167ec67a9f31f7
Naima-coder <69176965+Naima-coder@users.noreply.github.com>
2021-01-15 00:47:48 -0600

    Delete trial directory

--- trial/app1/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import student
-class form1(forms.Form):
-    name=forms.CharField(label="Name",max_length=100)
-    feedback=forms.CharField(label="FeedbaCK")
-class mform(forms.ModelForm):
-    class Meta:
-        model= student
-        fields=["name"]
--- trial/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-             path("login/",views.login_form,name="view1"),
-
-]
